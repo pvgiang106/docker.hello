ce99329515738c85977a2f1adbbae936ead2dc02
pvgiang106 <pvgiang106@gmail.com>
2021-10-16 00:26:36 +0700

    add azure key vault

--- src/main/java/azure/keyvault/SampleApplication.java
+++ src/main/java/azure/keyvault/SampleApplication.java
@@ -1,20 +1,11 @@
 package azure.keyvault;
 
-import java.io.IOException;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;	
-import okhttp3.WebSocket;
 
 @SpringBootApplication
 public class SampleApplication implements CommandLineRunner {
@@ -29,48 +20,55 @@ public class SampleApplication implements CommandLineRunner {
 	private static final String FIGHTING_ROOM = "FIGHTING_ROOM";
 	private static final String BATTLE_ROOM = "BATTLE_ROOM";
 	
+	@Value("${jdbcUrl}")
+	private String jdbcUrl;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
 	public void run(String... varl) throws Exception {        
-		for(int userNumber = fromUser; userNumber <= maxUser; userNumber++) {
+		System.out.println("Value of key jdbcUrl: " + jdbcUrl);
+//		for(int userNumber = fromUser; userNumber <= maxUser; userNumber++) {
 //			ResponseLogin response = login(userNumber);
-			conectWebsocket("accesstoken");
-			Thread.sleep(150);
-		}
+//			conectWebsocket("accesstoken");
+//			Thread.sleep(150);
+//		}
 	}
 
-	private ResponseLogin login(int userNumber) throws IOException {
-		OkHttpClient client = new OkHttpClient().newBuilder()
-				.build();
-		MediaType mediaType = MediaType.parse("application/json");
-		RequestLoginModel loginModel = buildBody(userNumber);
-		RequestBody body = RequestBody.create(mediaType, mapper.writeValueAsString(loginModel));
-		Request request = new Request.Builder()
-				.url(HTTP_HOST + LOGIN_URL)
-				.method("POST", body)
-				.addHeader("Content-Type", "application/json")
-				.build();
-		ResponseBody response =  client.newCall(request).execute().body();
-		return mapper.readValue(response.string(), ResponseLogin.class);
-	}
-
-	private void conectWebsocket(final String accessToken) throws JsonProcessingException{
-		WebSocketEcho wsEcho = new WebSocketEcho();
-		wsEcho.setWebSocketHost(WEBSOCKET_HOST);
-		WebSocket ws = wsEcho.start();
-		RequestWSLogin wsLogin = new RequestWSLogin();
-		wsLogin.setType(8);
-		wsLogin.addSource(accessToken);
-		wsLogin.addSource(BATTLE_ROOM);
-		ws.send(mapper.writeValueAsString(wsLogin));
-	}
-
-	private RequestLoginModel buildBody(int i) {
-		RequestLoginModel loginModel = new RequestLoginModel();
-		loginModel.setUsername("salus_user"  + i);
-		loginModel.setPassword("Salus@1234");
-		return loginModel;
-	}
+	
+	
+	
+//	private ResponseLogin login(int userNumber) throws IOException {
+//		OkHttpClient client = new OkHttpClient().newBuilder()
+//				.build();
+//		MediaType mediaType = MediaType.parse("application/json");
+//		RequestLoginModel loginModel = buildBody(userNumber);
+//		RequestBody body = RequestBody.create(mediaType, mapper.writeValueAsString(loginModel));
+//		Request request = new Request.Builder()
+//				.url(HTTP_HOST + LOGIN_URL)
+//				.method("POST", body)
+//				.addHeader("Content-Type", "application/json")
+//				.build();
+//		ResponseBody response =  client.newCall(request).execute().body();
+//		return mapper.readValue(response.string(), ResponseLogin.class);
+//	}
+//
+//	private void conectWebsocket(final String accessToken) throws JsonProcessingException{
+//		WebSocketEcho wsEcho = new WebSocketEcho();
+//		wsEcho.setWebSocketHost(WEBSOCKET_HOST);
+//		WebSocket ws = wsEcho.start();
+//		RequestWSLogin wsLogin = new RequestWSLogin();
+//		wsLogin.setType(8);
+//		wsLogin.addSource(accessToken);
+//		wsLogin.addSource(BATTLE_ROOM);
+//		ws.send(mapper.writeValueAsString(wsLogin));
+//	}
+//
+//	private RequestLoginModel buildBody(int i) {
+//		RequestLoginModel loginModel = new RequestLoginModel();
+//		loginModel.setUsername("salus_user"  + i);
+//		loginModel.setPassword("Salus@1234");
+//		return loginModel;
+//	}
 
 }
